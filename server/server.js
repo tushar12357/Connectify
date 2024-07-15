4e29c21a4676c48a464a5aa8fc1752cfa48ae208
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2024-07-15 23:41:20 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv  from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
+import { register } from './controllers/auth.js';
+import { createPost } from './controllers/posts.js';
+import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Posts.js';
+import { users,posts } from './data/index.js';
+//Configurations
+
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+dotenv.config()
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
+app.use('/assests',express.static(path.join(__dirname,'public/assets')));
+
+//file storage
+const storage=multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'public/assets');
+    },
+    filename:function(req,file,cb){
+        cb(null,file.originalname);
+    }
+});
+
+const upload=multer({storage});
+
+//routes with files
+app.post('/auth/register',upload.single('picture'),register);
+app.post('/posts',verifyToken,upload.single('picture'),createPost)
+
+//routes
+app.use('/auth',authRoutes)
+app.use('/users',userRoutes)
+app.use('/posts',postRoutes)
+
+//mongoose setup
+const PORT=process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    app.listen(PORT,()=>console.log(`Server listening on port: ${PORT}`));
+    console.log("MongoDB is connected")
+    // User.insertMany(users)
+    // Post.insertMany(posts)
+}).catch((error)=>console.log(`${error} did not connect`));
